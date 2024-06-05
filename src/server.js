5db6e47211d7cd72afca6a08d201010da644e801
skyeome <tjru7230@naver.com>
2024-06-05 15:54:39 +0900

    #2.5 Room message

--- src/server.js
+++ src/server.js
@@ -17,11 +17,13 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
   });
 });
 
