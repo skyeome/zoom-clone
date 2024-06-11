aad4fc5b354539a072fbd5ae3fbfefae9981f041
skyeome <tjru7230@naver.com>
2024-06-11 12:36:49 +0900

    #3.4 Join room

--- src/server.js
+++ src/server.js
@@ -16,4 +16,12 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
 httpServer.listen(3000, handleListen);
